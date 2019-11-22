e142e0b58eff90c47c5af00d9d9732f796ecbbce
vique254 <victoronyango051@gmail.com>
2019-11-22 13:07:18 +0300

    add authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,7 +16,6 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
 
         flash('Invalid username or Password')
-
     title = "watchlist login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
